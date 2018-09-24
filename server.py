f01a4b7d34cfed5cec57fd099441b410833ceeed
Gonzalo Ruiz-Oriol <gonzaloruizoriolgmail.com>
2018-09-24 20:48:54 +0200

    feature: minor edits

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, session, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
 import os
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.secret_key = "Le Password"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 #Conexion a la db
 
