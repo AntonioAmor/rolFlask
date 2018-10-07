c04d1e6ae276edfd9d7b5e932200e41ea5c01baf
antonioamor <antonio.amor.mourelle@gmail.com>
2018-10-07 22:35:18 +0200

    quieries:   get_games --> get_available_games   new_game   add_user_to_game   current_users_plus_one // usar al añadir un jugador a una partida
    allGames --> availableGames
    
    los diccionarios de partidas reciben todos los datos
    
    user_id annadido a session
    
    SIN PROBAR:
    
      /newGame
      /joinGame

--- server.py
+++ server.py
@@ -16,29 +16,36 @@ db = 'example.db'
 find_users = "SELECT * FROM users WHERE user_name = ?"
 get_id = "SELECT user_id FROM users WHERE user_name = ?"
 register_users = "INSERT INTO users (user_name, user_pswd) VALUES (?,?)"
-get_games = "SELECT * FROM game"
+get_available_games = "SELECT * FROM game WHERE current_users < max_users AND private = 0"
 get_user_games = "SELECT game from game_user WHERE myuser = ?"
+
+new_game = "INSERT INTO game (game_name, game_desc) VALUES (?,?)"
+
+add_user_to_game = "INSERT INTO game_user (myuser, game) VALUES (?,?)"
+
+current_users_plus_one = "UPDATE game SET current_users = current_users + 1 WHERE game_id = ?"
 
 @app.route('/')
 def inServer():
     if 'username' not in session:
         return redirect(url_for("login"))
-    dict = { "myGames" : [], "allGames" : [], "game_desc": []}
+    dict = { "myGames" : [], "availableGames" : [], "game_desc": []}
     myGames = []
     with sqlite3.connect(db) as conn:
         id = conn.cursor().execute(get_id, (session['username'],)).fetchone()[0]
         #We get the games the user is in
         for line in conn.cursor().execute(get_user_games, (id,)):
             myGames.append(line[0])
-        #We get all games
-        for line in conn.cursor().execute(get_games):
+        #We get all available and public games
+        for line in conn.cursor().execute(get_available_games):
+            print(line)
             #if its one of ours we flag it
             if line[0] in myGames: #FIXME: Possible error, we depend on the db schema not changing
-                dict['myGames'].append(line[1])
+                dict['myGames'].append(line[1:-1])
             #if its not we add it to the other list
-            #IDEA: public and private games
+            #TODO: public and private games
             else:
-                dict['allGames'].append((line[1], line[2]))
+                dict['availableGames'].append(line[1:-1])
     return render_template("index.html", dict=dict)
 
 @app.route('/get_user/<user>')
@@ -55,10 +62,12 @@ def login():
             hashedPwd = hashlib.sha224(request.form['pass'].encode("utf-8")).hexdigest()
             #we check the new user is not already registered
             for line in conn.cursor().execute(find_users, (username,)):
-                    if hashedPwd in line:
-                        session['username'] = str(request.form['user'])
-                        session['shownUser']  = session['username']
-                        return redirect('/')
+                print(line)
+                if hashedPwd in line:
+                    session['user_id'] = line[0]
+                    session['username'] = str(request.form['user'])
+                    session['shownUser']  = session['username']
+                    return redirect('/')
             return render_template('login.html')
     if 'username' in session:
         return redirect('/')
@@ -70,6 +79,8 @@ def logout():
         session.pop('username')
     if 'shownUser' in session:
         session.pop('shownUser')
+    if 'user_id' in session:
+        session.pop('user_id')
     return redirect('/')
 
 @app.route('/register', methods=['POST'])
@@ -90,6 +101,34 @@ def register():
             session['username'] = str(request.form['user'])
             session['shownUser']  = session['username']
             return redirect('/')
+
+
+# NOT TESTED
+@app.route('/newGame', methods=['POST'])
+def newGame():
+    if request.method == 'POST':
+        with sqlite3.connect(db) as conn:
+            game_name= request.form['game_name']
+            game_desc= reques.form['game_desc']
+
+            #Create the game
+            res=conn.cursor().execute(new_game, (game_name, game_desc))
+
+            print(res)
+            #Add user to game
+            conn.cursor().execute(add_user_to_game,(session['user_id'],game_id))
+
+
+# NOT TESTED
+@app.route('/joinGame', methods=['POST'])
+def joinGame():
+    if request.method == 'POST':
+        with sqlite3.connect(db) as conn:
+            #Add user to game
+            conn.cursor().execute(add_user_to_game,(session['user_id'],game_id))
+
+            return redirect('/')
+
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
